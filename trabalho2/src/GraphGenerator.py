a266c8ffaee483ea5e83cb95313cb829881c46df
João Vitor <joaovnevescosta@hotmail.com>
2025-10-30 23:08:29 -0300

    refactor: :sparkles: Clean up generate_bar_chart function by removing commented-out code

--- trabalho2/src/GraphGenerator.py
+++ trabalho2/src/GraphGenerator.py
@@ -14,21 +14,3 @@ def generate_bar_chart(data: Dict, title: str, xlabel: str, ylabel: str, filenam
     plt.ylabel(ylabel)
     plt.savefig(filename)
     plt.close()
-
-"""
-def generate_bar_chart(data: Dict, title: str, xlabel: str, ylabel: str, filename: str, colors: Dict = None):
-    
-    plt.figure(figsize=(10, 6))
-    bars = plt.bar(data.keys(), data.values(), color=[colors.get(k, 'blue') for k in data.keys()] if colors else 'blue')
-    
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    
-    for bar in bars:
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width()/2.0, yval, f'{yval:.2f}', va='bottom')  # va: vertical alignment
-    
-    plt.savefig(filename)
-    plt.close()
-"""
